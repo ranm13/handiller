8e911928fec2899550682a76d8193cfbab71297e
Ran Marciano <ranm13@gmail.com>
2019-08-27 13:53:36 +0300

    added all observables to the store

--- src/stores/SignUpStore.js
+++ src/stores/SignUpStore.js
@@ -2,7 +2,27 @@ import { observable, action } from  'mobx'
 import axios from 'axios'
 
 export class SignUpStore {
+    @observable isProfessional
+    @observable firstNameInput
+    @observable lastNameInput
+    @observable emailInput
+    @observable phoneInput
+    @observable passwordInput
+    @observable addressInput
+    @observable cityInput
+    @observable areasInput
+    @observable proffessionInput
+    @observable citiesList = []
+    @observable areasList = []
 
+    @action inputHandler = (name, value) => {
+        this[name] = value
+    } 
+
+
+    @action getCitiesAndAreas
+
+    @action
 }   
 
 
