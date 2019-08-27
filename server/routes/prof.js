c23c9da9afae68b03756c4f6c74431cd169b77dc
Ran Marciano <ranm13@gmail.com>
2019-08-27 17:12:03 +0300

    added dummy data to sql tables

--- server/routes/prof.js
+++ server/routes/prof.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const Sequelize = require('sequelize')
 
-router.get("/sanity", function (req, res) {
+const sequelize = new Sequelize('mysql://root:@localhost/handiller_DB')
+
+router.get("/prof-details/:profId", async function (req, res) {
+    let id = req.params.profId
     res.send("Works!")
 })
 
--- server/sql/create_tables.sql
+++ server/sql/create_tables.sql
@@ -84,3 +84,49 @@ USE handiller_DB;
 --     FOREIGN KEY(client_id) REFERENCES Clients(id)
 -- );
 
+-- INSERT INTO Cities (id, name) VALUES(null, "Ramat-Gan");
+-- INSERT INTO Cities (id, name) VALUES(null, "Haifa");
+-- INSERT INTO Cities (id, name) VALUES(null, "Tel Aviv");
+
+-- INSERT INTO Areas (id, name) VALUES(null, "Gush-Dan");
+-- INSERT INTO Areas (id, name) VALUES(null, "HaTzafon");
+-- INSERT INTO Areas (id, name) VALUES(null, "The West Bank");
+
+-- INSERT INTO Cities_areas (city_id, area_id) VALUES(1, 1);
+-- INSERT INTO Cities_areas (city_id, area_id) VALUES(2, 2);
+-- INSERT INTO Cities_areas (city_id, area_id) VALUES(3, 1);
+
+-- INSERT INTO Clients (id, first_name, last_name, email, phone, password, address, city_id) 
+-- VALUES(null, "Yossi", "Shamir", "ys@gmail.com", 0524473652, "hdjfhf133", "Hashavim 12", 3 );
+-- INSERT INTO Clients (id, first_name, last_name, email, phone, password, address, city_id) 
+-- VALUES(null, "Shosh", "Breer", "cgfg@gmail.com", 0532478762, "dhfdj20", "Hapotzim 36", 1 );
+-- INSERT INTO Clients (id, first_name, last_name, email, phone, password, address, city_id) 
+-- VALUES(null, "Moti", "Gibor", "mg36@gmail.com", 0534455447, "5236hshr", "y.b 43", 1 );
+
+-- INSERT INTO Professions (id, Profession) VALUES(null, "Air Conditioner Technician");
+-- INSERT INTO Professions (id, Profession) VALUES(null, "Scooter Technician");
+-- INSERT INTO Professions (id, Profession) VALUES(null, "Painter");
+
+-- INSERT INTO Professionals (id, first_name, last_name, email, phone, password, address, city_id, description)
+-- VALUES(null, "Kira", "Marco", "km@gmail.com", 0578893633, "dfghd22", "Brurim 2", 3 , "A friend, A brother");
+-- INSERT INTO Professionals (id, first_name, last_name, email, phone, password, address, city_id, description)
+-- VALUES(null, "Tal", "Museri", "tm243@gmail.com", 0538354463, "fdfd333r", "Hadror 26", 2 , "Boged");
+-- INSERT INTO Professionals (id, first_name, last_name, email, phone, password, address, city_id, description)
+-- VALUES(null, "Shreder", "Harasha", "turtleshater@gmail.com", 0549874362, "krain34", "Mimad X", 1 , "Atem olim li al ha-atzvim tzvim");
+
+-- INSERT INTO Professions_professionals (professional_id, profession_id) VALUES(1, 2);
+-- INSERT INTO Professions_professionals (professional_id, profession_id) VALUES(2, 1);
+-- INSERT INTO Professions_professionals (professional_id, profession_id) VALUES(3, 3);
+
+-- INSERT INTO Professionals_areas (professional_id, area_id) VALUES(1, 1);
+-- INSERT INTO Professionals_areas (professional_id, area_id) VALUES(1, 3);
+-- INSERT INTO Professionals_areas (professional_id, area_id) VALUES(2, 2);
+-- INSERT INTO Professionals_areas (professional_id, area_id) VALUES(3, 1);
+-- INSERT INTO Professionals_areas (professional_id, area_id) VALUES(3, 3);
+
+-- INSERT INTO Appointments (id, status, start_date, end_date, title, professional_id, client_id)
+-- VALUES(null, "approved", CURRENT_DATE(), CURRENT_DATE(), "meeting 1", 2, 3 );
+-- INSERT INTO Appointments (id, status, start_date, end_date, title, professional_id, client_id)
+-- VALUES(null, "canceld", CURRENT_DATE(), CURRENT_DATE(), "meeting 2", 1, 2 );
+-- INSERT INTO Appointments (id, status, start_date, end_date, title, professional_id, client_id)
+-- VALUES(null, "pending", CURRENT_DATE(), CURRENT_DATE(), "meeting 3", 2, 1 );
