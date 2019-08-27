85b2462e6353c8b47a33e56d87adfc8b1d669343
Itamar Bell <itamarbell@Itamars-MacBook-Pro.local>
2019-08-27 18:19:45 +0300

    calendar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './styles/App.css';
 import { observer } from 'mobx-react'
 import ClientHomePage from './components/Clients/ClientHomePage';
 import ProfHomePage from './components/Pros/ProfHomePage';
-
+import Date from './components/Date';
+// import Calendar from 'react-calendar';
 
 @observer
-class App extends Component{
-  render(){
-      return (
-    <div className="App">
-      <ClientHomePage />
-      <ProfHomePage />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Date />
+        <ClientHomePage />
+        <ProfHomePage />
+      </div>
+    );
   }
 
 }
---
+++ src/components/Pros/Requests.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { observer } from 'mobx-react'
+
+@observer
+class Requests extends Component {
+    render() {
+        return (
+        <div>
+    
+        </div>)
+    }
+}
+export default Requests
