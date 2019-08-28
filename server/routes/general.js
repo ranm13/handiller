e0b91a302b2862155576959eb7c60445675462a2
Ran Marciano <ranm13@gmail.com>
2019-08-28 13:55:53 +0300

    done with the post route

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Sequelize = require('sequelize')
+const moment = require('moment')
+// Override timezone formatting for MSSQL
+Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
+    return this._applyTimezone(date, options).format('YYYY-MM-DD HH:mm:ss.SSS');
+};
+const sequelize = new Sequelize('mysql://root:@localhost/handiller_db')
 
 router.get("/professionals", function (req, res) {
     
 })
 
-router.get("/appointment/:id?isClient", function (req, res) {
-    const id = req.params.id;
-    let isClient = false;
-
-    if(req.query.isClient === "true")
-        isClient = true;
-    
-})
 
 router.get("/appointments/:id", function (req, res) {
     const id = req.params.id;
+    const isClient = req.query.isClient;
     
 })
 
@@ -27,23 +27,38 @@ router.get("/regions", function (req, res) {
     
 })
 
-router.get("/citiesByRegion/:region", function (req, res) {
-    const region = req.params.region;
-
-})
-
-router.get("/regionByCity/:city", function (req, res) {
-    const city = req.params.city;
-
-})
-
-router.post("/appointment", function (req, res) {
+router.post("/appointment",async function (req, res) {
     const data = req.body;
-    
+    const startDate = moment(new Date(data.start)).subtract(3, "hours").format("YYYY-MM-DD HH:mm:ss.SSS")
+    const endDate = moment(new Date(data.end)).subtract(3, "hours").format("YYYY-MM-DD HH:mm:ss.SSS")
+    let query =`INSERT INTO Appointments VALUES ( null, "pending", '${startDate}', '${endDate}', '${data.title}', ${data.pro_id}, ${data.cli_id});`
+    await sequelize.query(query)
+    res.end()
 })
 
-router.put("/update-status/:appointmentId", function (req, res) {
-    
+router.put("/update-status/:appointmentId", async function (req, res) {
+    const appointmentId = req.params.appointmentId
+    const data = req.body;
+    console.log(appointmentId)
+    console.log(data)
+    const startDate = moment(new Date(data.start)).subtract(3, "hours").format("YYYY-MM-DD HH:mm:ss.SSS")
+    const endDate = moment(new Date(data.end)).subtract(3, "hours").format("YYYY-MM-DD HH:mm:ss.SSS")
+    let query =`UPDATE Appointments 
+                SET status = '${data.status}',
+                start_date = '${startDate}', 
+                end_date = '${endDate}', 
+                title = '${data.title}'
+                WHERE id = ${appointmentId};`
+    await sequelize.query(query)
+    res.end()
 })
 
 module.exports = router;
+
+// const insertCity = async function(cityName, areaId){
+//     let query =`INSERT INTO cities VALUES ( null, "${cityName}", ${areaId});`
+//     let result = await sequelize.query(query)
+//     return result[0]
+// }
+// (null, "approved", CURRENT_DATE(), CURRENT_DATE(), "meeting 1", 2, 3 )
+// Appointments (id, status, start_date, end_date, title, professional_id, client_id)
