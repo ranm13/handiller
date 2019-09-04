373cbd598a0720af5dd785e03550bbd265325ec3
DESKTOP-3DN4RNO\idank <idankcompany@gmail.com>
2019-09-04 14:25:48 +0300

    before merge

--- src/components/Pros/ProfHome.js
+++ src/components/Pros/ProfHome.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
 import { Grid } from '@material-ui/core'
 import CalendarComp from './CalendarComp'
 import Requests from './Requests'
 
 
-
+@observer
+@inject("professionalStore")
+@inject('logInStore')
+@inject('logInStore')
 class ProfHome extends Component {
+
+    componentDidMount() {
+        this.props.clientStore.getPersonalData(this.props.logInStore.userId)
+        this.props.professionalStore.getPersonalData(this.props.logInStore.user.userId)
+      }
+
     render() {
         return (
-            <Grid container spacing={2} style={{margin: "1.5vh 0.1vw"}}>
+            <Grid container spacing={1} >
                 <Grid item xs={9}>
-                  <CalendarComp />
+                    <CalendarComp />
                 </Grid>
                 <Grid item xs={3}> 
                     <Requests /> 
